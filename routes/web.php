8a5212cc7383d855a0702d8f93c9589015337f0a
estagiario.cti <estagiario.cti@paranacidade.org.br>
2026-06-17 13:17:57 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/oi', function () {
+    echo "Oi";
+});
+Route::get('/oi/oo', function () {
+    echo "Oo";
+});
