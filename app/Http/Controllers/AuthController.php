80edf9dd05a72e4e5bae72bf68989bcebdcbd95b
estagiario.cti <estagiario.cti@paranacidade.org.br>
2026-06-17 17:32:02 -0300

    m

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,16 @@ class AuthController extends Controller
 {
     public function login()
     {
-        echo 'login';
+        return view('login');
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        echo 'login submit';
     }
 
     public function logout()
     {
-        echo 'logout';
+
     }
 }
