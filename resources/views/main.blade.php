c6c49cf10c5a4d3b5c06d6d3b8a89421959f84d3
estagiario.cti <estagiario.cti@paranacidade.org.br>
2026-06-17 14:21:18 -0300

    Limpeza

--- resources/views/main.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout.main_layout')
-@section('content')
-    <h1>Welcome View and Blade</h1>
-    <hr>
-    <h3>The value is {{$value}}</h3>
-@endsection
--- resources/views/page3.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.main_layout')
-@section('content')
-    <h1>Welcome View and Blade</h1>
-    <hr>
-    <h2>Page 3</h2>
-    <h3>The value is {{$value}}</h3>
-@endsection
