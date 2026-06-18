9960c68fae79355aa69df103f3106f2baa8534da
estagiario.cti <estagiario.cti@paranacidade.org.br>
2026-06-18 15:39:31 -0300

    a

---
+++ app/Http/Middleware/CheckIsLogged.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckIsLogged
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // check if user is logged
+        if(!session('user')){
+            return redirect('/login');
+        }
+
+        return $next($request);
+    }
+}
